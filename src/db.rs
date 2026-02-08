a8a7182d2ca965a3b5b0c3ce1fba559fb33112d0
Bucky <60587749+Stinky-c@users.noreply.github.com>
2026-02-08 13:57:25 -0800

    Partial blanket impl for migrations and generic connections.

--- src/db.rs
+++ src/db.rs
@@ -1,161 +1,260 @@
-#[cfg(all(feature = "postgres", feature = "mssql"))]
-compile_error!("feature \"postgres\" and feature \"mssql\" cannot be enabled at the same time");
+use color_eyre::{Report, eyre::Result};
+use refinery::config::{Config, ConfigDbType};
+use sql_middleware::DatabaseType;
+use std::env::var;
+use std::fmt::{Debug, Formatter};
+use std::result::Result as StdResult;
+use std::str::FromStr;
+
+#[cfg(not(any(
+    feature = "postgres",
+    feature = "sqlite",
+    feature = "turso",
+    feature = "mssql"
+)))]
+compile_error!("Requires at least one db driver.");
 
 pub(crate) mod embedded {
-    use refinery::embed_migrations;
-    use refinery::{Error, Report,};
-    use sql_middleware::MiddlewarePoolConnection;
-    use crate::db::wrap;
+    use crate::db::{DbConfig, DbType};
+    use color_eyre::{Report, eyre, eyre::Result};
+    use refinery::{Report as RunnerReport, config::Config};
 
-    embed_migrations!("migrations");
+    // Dynamically compile in migration for each driver
 
-    pub(crate) async fn run_async(conn: MiddlewarePoolConnection) -> Result<Report, Error> {
-        let runner = migrations::runner();
-        runner.run_async(&mut wrap(conn)).await
+    #[cfg(feature = "postgres")]
+    pub(crate) mod postgres {
+        refinery::embed_migrations!("migrations/postgres");
     }
-}
 
-// NOTE: Maybe break out into separate crate.
-use refinery::{AsyncMigrate, Migration};
-use refinery_core::traits::r#async::{AsyncQuery, AsyncTransaction};
-use sql_middleware::{CustomDbRow, MiddlewarePoolConnection, SqlMiddlewareDbError};
-
-/// Wrap MiddlewarePoolConnection
-struct MiddlewarePoolConnectionWrapper(MiddlewarePoolConnection);
-
-fn migration_from_row(row: CustomDbRow) -> Result<Migration, SqlMiddlewareDbError> {
-    let version: i32 = {
-        let row = match row.get_by_index(0) {
-            Some(row) => Ok(row),
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get index 0".to_string(),
-            )),
-        }?;
-
-        match row.as_int() {
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get value".to_string(),
-            )),
-            Some(value) => Ok(value.to_owned() as i32),
-        }?
-    };
-    let name: String = {
-        let row = match row.get_by_index(1) {
-            Some(row) => Ok(row),
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get index 1".to_string(),
-            )),
-        }?;
-
-        match row.as_text() {
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get value".to_string(),
-            )),
-            Some(value) => Ok(value.to_owned()),
-        }?
-    };
-    let applied_on: time::OffsetDateTime = {
-        let row = match row.get_by_index(2) {
-            Some(row) => Ok(row),
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get index 2".to_string(),
-            )),
-        }?;
-
-        match row.as_timestamp() {
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get value".to_string(),
-            )),
-            // TODO: ensure this has the right timestamp
-            Some(value) => time::OffsetDateTime::from_unix_timestamp(value.and_utc().timestamp())
-                .map_err(|_| {
-                    SqlMiddlewareDbError::Unimplemented("Failed to convert value".to_string())
-                }),
-        }?
-    };
-    let checksum: u64 = {
-        let row = match row.get_by_index(3) {
-            Some(row) => Ok(row),
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get index 0".to_string(),
-            )),
-        }?;
-
-        match row.as_int() {
-            None => Err(SqlMiddlewareDbError::Unimplemented(
-                "Failed to get value".to_string(),
-            )),
-            Some(value) => Ok(value.to_owned() as u64),
-        }?
-    };
-    Ok(Migration::applied(version, name, applied_on, checksum))
-}
-
-// An async query that expects to return a vec of migrations
-impl AsyncQuery<Vec<Migration>> for MiddlewarePoolConnectionWrapper {
-    async fn query(&mut self, query: &str) -> Result<Vec<Migration>, Self::Error> {
-        // TODO: impl starting a transaction
-        let value = self.0.query(query).select().await?;
-        let mut migrations: Vec<Migration> = vec![];
-
-        for row in value.results {
-            let migration = migration_from_row(row)?;
-            migrations.push(migration);
-        }
-
-        Ok(migrations)
+    #[cfg(any(feature = "sqlite", feature = "turso"))]
+    pub(crate) mod sqlite {
+        refinery::embed_migrations!("migrations/sqlite");
     }
-}
 
-// Begin a transaction and run queries
-impl AsyncTransaction for MiddlewarePoolConnectionWrapper {
-    type Error = SqlMiddlewareDbError;
+    #[cfg(feature = "mssql")]
+    pub(crate) mod mssql {
+        suprefineryer::embed_migrations!("migrations/mssql");
+    }
 
-    async fn execute<'a, T: Iterator<Item = &'a str> + Send>(
-        &mut self,
-        queries: T,
-    ) -> Result<usize, Self::Error> {
-        match self.0 {
-            #[cfg(feature = "mssql")]
-            MiddlewarePoolConnection::Mssql { conn, .. } => {
-                let mut tx = sql_middleware::mssql::begin_transaction(&mut *conn).await?;
-                let mut count = 0;
-                for i in queries {
-                    tx.execute_batch(i).await?;
-                    count += 1;
-                }
+    pub(crate) async fn run_async(db_config: DbConfig) -> Result<RunnerReport> {
+        let db_type = db_config.db_type;
+        let mut config: Config = db_config.try_into().map_err(Report::msg)?;
 
-                let _ = tx.commit().await?;
-                Ok(count as usize)
-            }
+        match db_type {
+            // Migrations are not compiled in if feature disabled
             #[cfg(feature = "postgres")]
-            MiddlewarePoolConnection::Postgres { client, .. } => {
-                let mut tx = sql_middleware::postgres::begin_transaction(client).await?;
-                let mut count = 0;
-                for i in queries {
-                    tx.execute_batch(i).await?;
-                    count += 1;
-                }
-                let _ = tx.commit().await?;
-                Ok(count as usize)
+            DbType::Postgres => {
+                let runner = postgres::migrations::runner();
+                Ok(runner.run_async(&mut config).await?)
             }
-            _ => unimplemented!("Too lazy to add."),
+            #[cfg(any(feature = "sqlite", feature = "turso"))]
+            DbType::Sqlite | DbType::Turso => {
+                let runner = sqlite::migrations::runner();
+                Ok(runner.run(&mut config)?)
+            }
+            #[cfg(feature = "mssql")]
+            DbType::Mssql => {
+                let runner = mssql::migrations::runner();
+                Ok(runner.run_async(&mut config).await?)
+            }
+
+            _ => Err(eyre::eyre!("Unsupported database type {:?}", db_type)),
         }
     }
 }
 
-impl AsyncMigrate for MiddlewarePoolConnectionWrapper {}
+/// Type of Database. One of `postgres`, `pgsql`, `sqlite`, `mssql`, `turso`.
+static ENV_DB_TYPE: &str = "DB_TYPE";
+/// Hostname or ip to connect to.
+static ENV_DB_HOST: &str = "DB_HOST";
+/// Port to connect to.
+static ENV_DB_PORT: &str = "DB_PORT";
+/// Database to connect to.
+static ENV_DB_NAME: &str = "DB_NAME";
+/// Database Username.
+static ENV_DB_USER: &str = "DB_USER";
+/// Database password.
+static ENV_DB_PASS: &str = "DB_PASS";
+/// Database file path. Used for sqlite.
+static ENV_DB_PATH: &str = "DB_PATH";
 
-impl std::ops::Deref for MiddlewarePoolConnectionWrapper {
-    type Target = MiddlewarePoolConnection;
-    fn deref(&self) -> &Self::Target {
-        &self.0
+#[derive(Clone)]
+pub(crate) struct DbConfig {
+    pub(crate) db_type: DbType,
+    pub(crate) host: Option<String>,
+    pub(crate) port: Option<u16>,
+    pub(crate) db_name: Option<String>,
+    pub(crate) user_name: Option<String>,
+    pub(crate) password: Option<String>,
+    pub(crate) path: Option<String>,
+}
+
+// Hide password from debug
+impl Debug for DbConfig {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("DbConfig")
+            .field("db_type", &self.db_type)
+            .field("host", &self.host)
+            .field("port", &self.port)
+            .field("db_name", &self.db_name)
+            .field("username", &self.user_name)
+            .field("path", &self.path)
+            .finish()
     }
 }
 
-/// Wrap MiddlewarePoolConnection into a type for handling
-pub fn wrap(conn: MiddlewarePoolConnection) -> MiddlewarePoolConnectionWrapper {
-    MiddlewarePoolConnectionWrapper(conn)
+impl DbConfig {
+    pub(crate) fn from_env() -> Result<Self> {
+        let db_type = var(ENV_DB_TYPE)?.parse::<DbType>().map_err(Report::msg)?;
+
+        let port: Option<u16> = match var(ENV_DB_PORT) {
+            Ok(value) => match value.as_str() {
+                "" => None,
+                v if !v.is_empty() => Some(value.parse().map_err(Report::msg)?),
+                _ => None, // Should be unreachable, but just in case
+            },
+            Err(_) => None,
+        };
+
+        Ok(Self {
+            db_type,
+            host: var(ENV_DB_HOST).ok(),
+            port,
+            db_name: var(ENV_DB_NAME).ok(),
+            user_name: var(ENV_DB_USER).ok(),
+            password: var(ENV_DB_PASS).ok(),
+            path: var(ENV_DB_PATH).ok(),
+        })
+    }
 }
 
+#[cfg(feature = "sqlite")]
+impl TryFrom<DbConfig> for sql_middleware::SqliteOptions {
+    type Error = String;
 
+    fn try_from(config: DbConfig) -> StdResult<Self, Self::Error> {
+        if config.db_type != DbType::Sqlite {
+            return Err("DbType is not sqlite".to_string());
+        }
+
+        Ok(Self::new(config.path.ok_or("Could not find path")?))
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl TryFrom<DbConfig> for sql_middleware::PgConfig {
+    type Error = String;
+
+    fn try_from(config: DbConfig) -> StdResult<Self, Self::Error> {
+        if config.db_type != DbType::Postgres {
+            return Err("DbType is not postgres".to_string());
+        }
+        let mut pg = sql_middleware::PgConfig::new();
+        pg.host = config.host;
+        pg.port = config.port;
+        pg.dbname = config.db_name;
+        pg.user = config.user_name;
+        pg.password = config.password;
+        Ok(pg)
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl TryFrom<DbConfig> for sql_middleware::PostgresOptions {
+    type Error = String;
+    fn try_from(config: DbConfig) -> StdResult<Self, Self::Error> {
+        if config.db_type != DbType::Postgres {
+            return Err("DbType is not postgres".to_string());
+        }
+        let pg = config.try_into()?;
+
+        Ok(Self::new(pg))
+    }
+}
+
+#[cfg(feature = "mssql")]
+impl TryFrom<DbConfig> for sql_middleware::MssqlOptions {
+    type Error = String;
+    fn try_from(config: DbConfig) -> StdResult<Self, Self::Error> {
+        if config.db_type != DbType::Mssql {
+            return Err("DbType is not Mssql".to_string());
+        }
+        Ok(Self::new(
+            config.host.ok_or("Missing host")?,
+            config.db_name.ok_or("Missing db_name")?,
+            config.user_name.ok_or("Missing username")?,
+            config.password.ok_or("Missing Password")?,
+            config.port,
+            None,
+        ))
+    }
+}
+
+impl TryFrom<DbConfig> for Config {
+    type Error = String;
+    fn try_from(value: DbConfig) -> StdResult<Self, Self::Error> {
+        match value.db_type {
+            DbType::Postgres => {
+                unimplemented!() // TODO: Complete impl
+            }
+            DbType::Sqlite => {
+                Ok(Config::new(ConfigDbType::Sqlite)
+                    .set_db_path(&value.path.ok_or("Missing path")?))
+            }
+            DbType::Mssql => {
+                unimplemented!() // TODO: Complete impl
+            }
+            DbType::Turso => {
+                unimplemented!() // TODO: Complete impl
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub(crate) enum DbType {
+    Postgres,
+    Sqlite,
+    Mssql,
+    Turso,
+}
+
+impl FromStr for DbType {
+    type Err = String;
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value.to_lowercase().as_str() {
+            "postgres" | "pgsql " => Ok(Self::Postgres),
+            "mssql" => Ok(Self::Mssql),
+            "sqlite" => Ok(Self::Sqlite),
+            "turso" => Ok(Self::Turso),
+            _ => Err(format!("Unknown database type: {}", value)),
+        }
+    }
+}
+
+impl From<DbType> for DatabaseType {
+    fn from(value: DbType) -> Self {
+        match value {
+            #[cfg(feature = "postgres")]
+            DbType::Postgres => Self::Postgres,
+            #[cfg(feature = "sqlite")]
+            DbType::Sqlite => Self::Sqlite,
+            #[cfg(feature = "turso")]
+            DbType::Turso => Self::Sqlite,
+            #[cfg(feature = "mssql")]
+            DbType::Mssql => Self::Mssql,
+            _ => unimplemented!(),
+        }
+    }
+}
+impl From<DbType> for ConfigDbType {
+    fn from(value: DbType) -> Self {
+        match value {
+            DbType::Postgres => Self::Postgres,
+            DbType::Sqlite => Self::Sqlite,
+            DbType::Mssql => Self::Mssql,
+            DbType::Turso => Self::Sqlite,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 mod db;
+use crate::db::DbConfig;
+use color_eyre::Report;
+use color_eyre::eyre::Result;
 use sql_middleware::prelude::*;
-use std::env::var;
-use refinery::AsyncMigrate;
 
 #[tokio::main]
-async fn main() {
-    let host = var("DB_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let port = var("DB_PORT").unwrap_or_else(|_| "5432".to_string());
-    let username = var("DB_USER").unwrap_or_else(|_| "postgres".to_string());
-    let password = var("DB_PASSWORD").unwrap_or_else(|_| "password".to_string());
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    color_eyre::install()?;
 
+    let cfg = DbConfig::from_env()?;
 
-    let v = ConfigAndPool::new_mssql().await.unwrap();
+    // TODO: Build pool based on `DbType` and `DbConfig`
+    let pool = ConfigAndPool::new_sqlite(cfg.clone().try_into().map_err(Report::msg)?).await?; // FIXME
 
-    let mut conn = v.get_connection().await.unwrap();
+    let report = db::embedded::run_async(cfg).await?;
+    println!("{:?}", report);
 
-    let runner = db::embedded::migrations::runner();
-    let v = runner.run_async(&mut db::wrap(conn)).await; // Untested
+    let mut conn = pool.get_connection().await?;
 
     let query = QueryAndParams::new_without_params("SELECT * FROM users");
     let res = conn.query(&query.query).select().await?;
-}
+    println!("{:?}", res);
 
+    Ok(())
+}
